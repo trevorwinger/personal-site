7f68ba2727215ca5d113fc6d61765c20e6eb4e10
trevorwinger <winge159@umn.edu>
2021-02-10 23:53:20 -0600

    changed a lot of things

---
+++ src/components/Pages/Resume.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Resume = () => 
+{
+    const sections = {
+
+    }
+
+    return(
+        <div>
+            <p>RESUME</p>
+        </div>
+    )
+}
+
+export default Resume;
